0086dee8e6ee23caee030b0b6912491795af3fb5
Johanna <2myadverts@gmail.com>
2020-08-19 15:32:41 +0200

    adjusting PORT

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.set('view engine', 'hbs');
 const router = require('./config/routes.js');
 app.use('/', router);
 
-app.listen(3000, () => {
-  console.log('Ready on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Ready!');
 });
